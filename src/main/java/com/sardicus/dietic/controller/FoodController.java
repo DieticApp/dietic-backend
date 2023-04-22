8dc23baa1d50947e12a3b0611c81a32b27ebb02e
Enes Bilgin <enesbilgin@eskisehir.edu.tr>
2023-04-22 18:23:49 +0300

    files reformatted

---
+++ src/main/java/com/sardicus/dietic/controller/FoodController.java
@@ -0,0 +1,24 @@
+package com.sardicus.dietic.controller;
+
+import com.sardicus.dietic.entity.Food;
+import com.sardicus.dietic.service.FoodService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/v1/foods")
+@RequiredArgsConstructor
+public class FoodController {
+
+    private final FoodService foodService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Food>> searchFoods(@RequestParam("query") String query){
+        return ResponseEntity.ok(foodService.searchFoods(query));
+    }
+}
