d87d9e57292a80e35897e374a54ad7a7ceb812e2
Enes Bilgin <enesbilgin@eskisehir.edu.tr>
2023-05-10 22:19:26 +0300

    picture field added

--- src/main/java/com/sardicus/dietic/entity/User.java
+++ src/main/java/com/sardicus/dietic/entity/User.java
@@ -20,10 +20,12 @@ public class User {
     private String surname;
     private String email;
     private String password;
-
+    private String picture;
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Set<Role> roles;
+
+
 }
